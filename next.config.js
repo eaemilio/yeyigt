23b471c9a248cfab9ac0b229f243aa45749b49b1
Emilio Barrientos <ebarrientos@mangochango.com>
2022-09-08 06:34:07 -0600

    ignore eslint for builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-    reactStrictMode: true,
-    images: {
-        domains: ['lh3.googleusercontent.com', 'supabase-cdn.vercel.app'],
-    },
+  reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com', 'supabase-cdn.vercel.app'],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
